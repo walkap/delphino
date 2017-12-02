24a5f12af2d5428eb2c72430100427730ba412c1
riccardo <riccardogamucci@alice.it>
2017-12-02 18:05:35 +0100

    Added ReadConfig class

---
+++ src/dao/ReadConfig.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadConfig {
+
+
+    private String user, password, DbUrl, DriverUrl;
+
+    public void load() {
+
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream(/*path/filename*/"config.properties"));
+        } catch (IOException e) {
+
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+
+        this.user = properties.getProperty("USER");
+        this.password = properties.getProperty("PASSWORD");
+        this.DbUrl = properties.getProperty("DB_URL");
+        this.DriverUrl = properties.getProperty("DRIVER_CLASS_NAME");
+
+
+    }
+
+    public String getUser() {
+        return this.user;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getDbUrl() {
+        return this.DbUrl;
+    }
+
+    public String getDriverUrl() {
+        return this.DriverUrl;
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import dao.ReadConfig;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,6 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
