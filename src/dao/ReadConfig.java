740b572e3e98b142535d46fb52e3982af54eb55e
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-03 11:28:48 +0100

    --

--- src/dao/ReadConfig.java
+++ src/dao/ReadConfig.java
@@ -6,14 +6,13 @@ import java.util.Properties;
 
 public class ReadConfig  {
 
-
-    private String user, password, DbUrl, DriverUrl;
+    private String user, password, dbUrl, driverUrl;
 
     public void load() {
 
         Properties properties = new Properties();
         try {
-            properties.load(new FileInputStream(/*path/filename*/"config.properties"));
+            properties.load(new FileInputStream("config.properties"));
         } catch (IOException e) {
 
             System.out.println("File not found");
@@ -22,9 +21,8 @@ public class ReadConfig  {
 
         this.user = properties.getProperty("USER");
         this.password = properties.getProperty("PASSWORD");
-        this.DbUrl = properties.getProperty("DB_URL");
-        this.DriverUrl = properties.getProperty("DRIVER_CLASS_NAME");
-
+        this.dbUrl = properties.getProperty("DB_URL");
+        this.driverUrl = properties.getProperty("DRIVER_CLASS_NAME");
 
     }
 
@@ -37,10 +35,11 @@ public class ReadConfig  {
     }
 
     public String getDbUrl() {
-        return this.DbUrl;
+        return this.dbUrl;
     }
 
     public String getDriverUrl()  {
-        return this.DriverUrl;
+        return this.driverUrl;
     }
+
 }
