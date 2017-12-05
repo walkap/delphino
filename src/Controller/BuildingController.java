9e56646378664f377243930c0b46601843bcf6eb
riccardo <riccardogamucci@alice.it>
2017-12-05 19:32:14 +0100

    Controller for Building Creation

---
+++ src/Controller/BuildingController.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import Entity.Building;
+import dao.BuildingDao;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class BuildingController {
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField floorsNum;
+
+    public void createBuilding() {
+
+        String nameBuilding = name.getText();
+        int floorsNumber = Integer.parseInt(floorsNum.getText());
+
+        Building building = new Building(nameBuilding, floorsNumber);
+        BuildingDao buildingDao = new BuildingDao();
+        buildingDao.addBuilding(building);
+    }
+
+}
