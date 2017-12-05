fba32806f0d05e0c519560bf2165a9615742f25d
riccardo <riccardogamucci@alice.it>
2017-12-05 19:39:32 +0100

    DAO with method for adding a bulding in a database

---
+++ src/dao/BuildingDao.java
@@ -0,0 +1,41 @@
+package dao;
+
+import Entity.Building;
+
+import java.sql.*;
+
+public class BuildingDao {
+
+    private Connection connection;
+
+
+    public void addBuilding(Building building) {
+
+        Statement stmt = null;
+        DataSource src = new DataSource();
+        connection = src.getConnection();
+        try {
+            stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);
+            String sql = "insert into  Buildings(name, numberFloors) values ('" + building.getName() +
+                    "'," + building.getNumbersOfFloors() + ")";
+            stmt.executeQuery(sql);
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+        try {
+            connection.close();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main (String args[]) {
+        Building building = new Building("Headbuilding", 10);
+        BuildingDao buildingDao = new BuildingDao();
+        buildingDao.addBuilding(building);
+    }
+
+}
