1217d364fe18ef0f490e4ecc0f99766dc6b39427
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-07 20:15:30 +0100

    fixed package name

--- src/Controller/IssueHandlingController.java
+++ src/Controller/IssueHandlingController.java
@@ -1,24 +1,5 @@
-package Controller;
-
-import Entity.Issue;
-import Entity.Room;
-import Entity.User;
-
-import java.util.Date;
+package controller;
 
 public class IssueHandlingController {
 
-    private User user;
-    private int id;
-    private Date start;
-    private Date end;
-    private String description;
-    private Room room;
-
-    public void createIssue(int id, Date start, Date end, String description, Room room){
-
-        Issue issue = new Issue(id, start, end, description, room);
-
-
-    }
 }
