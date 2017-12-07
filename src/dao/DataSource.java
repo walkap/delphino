d90e5f2ae3cf74477dee7469943e0db75d6f8e82
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-07 19:58:57 +0100

    Improved readability, and moved some variables inside the method because those variables are used just to create a connection and not to close it

--- src/dao/DataSource.java
+++ src/dao/DataSource.java
@@ -7,36 +7,34 @@ import java.util.Properties;
 
 public class DataSource  {
 
+    private Connection connection;
+
     /**
+     *
      * Returns a connection to the database
+     *
      * @return Connection
      *
      */
-    private Properties properties;
-
-    private Connection connection;
-
-    private String USER, PASSWORD, DB_URL, DRIVER;
-
 
     public Connection getConnection() {
 
-        properties = ReadConfig.getProperties();
+        Properties properties = ReadConfig.getProperties();
 
-        USER = properties.getProperty("USER");
-        PASSWORD = properties.getProperty("PASSWORD");
-        DB_URL = properties.getProperty("DB_URL");
-        DRIVER = properties.getProperty("DRIVER_CLASS_NAME");
+        String user = properties.getProperty("USER");
+        String password = properties.getProperty("PASSWORD");
+        String dbUrl = properties.getProperty("DB_URL");
+        String driver = properties.getProperty("DRIVER_CLASS_NAME");
+
        try {
-           Class.forName(DRIVER);
-           connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+           Class.forName(driver);
+           connection = DriverManager.getConnection(dbUrl, user, password);
        }
        catch (SQLException | ClassNotFoundException e){
-
            e.printStackTrace();
-
        }
-        return connection;
+
+       return connection;
     }
 
 }
