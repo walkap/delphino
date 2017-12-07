cab7315bb57e93f088aa8c8f438f76f0355e473e
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-07 23:07:10 +0100

    used Singleton, splitted method in multiple methods to improve readability and maintainability

--- src/dao/ReadConfig.java
+++ src/dao/ReadConfig.java
@@ -4,44 +4,78 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class ReadConfig  {
+public class ReadConfig {
 
     private static final String FILE = "config.properties";
     private static Properties properties;
+    private FileInputStream fileInputStream;
+    private static ReadConfig instance = null;
 
     /**
+     * Pivate Default constructor avoid instantiation
+     * from external class
+     */
+
+    private ReadConfig(){}
+
+    /**
+     * Signleton to have a single instance of this class
+     * @return ReadConfig
+     */
+
+    private static synchronized ReadConfig getInstance(){
+        if(instance == null){
+            instance = new ReadConfig();
+        }
+        return instance;
+    }
+
+    /**
+     * Open a file stream
      *
+     * @return FileInputStream
+     * @throws IOException
+     */
+
+    private FileInputStream openStream() throws IOException {
+        fileInputStream = new FileInputStream(FILE);
+        return fileInputStream;
+    }
+
+    /**
+     * Close the file stream
+     *
+     */
+
+    private void closeStream() {
+        try{
+            if (fileInputStream != null)
+                fileInputStream.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
      * Load database parameter from configuration file,
      * then return a type Properties that contain an array
      *
      * @return Properties
-     *
      */
 
     public static Properties getProperties() {
-
-        FileInputStream fileInputStream = null;
-
-        //Try to open a file stream
         try {
-            fileInputStream = new FileInputStream(FILE);
             properties = new Properties();
-            properties.load(fileInputStream);
-
+            properties.load(getInstance().openStream());
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            try{
-                if(fileInputStream != null)
-                    fileInputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            getInstance().closeStream();
         }
         return properties;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Properties p = ReadConfig.getProperties();
         System.out.println(p);
     }
