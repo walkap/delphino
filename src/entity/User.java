128b8be9c6259341e90732a343ac2fb5633415b7
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-07 20:00:13 +0100

    fixed the package name

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,8 +1,42 @@
 package entity;
 
-public class User {
+/**This is a generic structure for a User in own application
+ * It will be finished in next iteration
+ */
 
+public class User {
+    private int ID;
     private String name;
-    private String emailAddress;
-    private String role;
+    private String surname;
+
+    public User(int ID, String name, String surname){
+        this.ID = ID;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
 }
