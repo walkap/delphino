0cd36f245d37eeee7f7688802f4418c94e391c23
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-13 20:06:14 +0100

    Deleted useless main method, some other improvements

--- src/entity/room/builder/RoomDirectorBuilder.java
+++ src/entity/room/builder/RoomDirectorBuilder.java
@@ -1,21 +1,19 @@
 package entity.room.builder;
 
-import entity.Building;
 import entity.room.Room;
 
 public class RoomDirectorBuilder {
-
     private RoomBuilder myRoomBuilder;
 
-    public Room getRoom()
-    {
-        return myRoomBuilder.getRoom();
-    }
-
-    public void buildRoom(String name, String building, String type)
-    {
-
-        switch (type){
+    /**
+     * This method create a new instance of room
+     *
+     * @param name     - String
+     * @param building - String
+     * @param type     - String
+     */
+    public void buildRoom(String name, String building, String type) {
+        switch (type) {
             case "ClassRoom":
                 myRoomBuilder = new ClassRoomBuilder();
                 break;
@@ -26,31 +24,16 @@ public class RoomDirectorBuilder {
                 myRoomBuilder = new CongressHallBuilder();
                 break;
         }
-
         myRoomBuilder.createRoom(name, building, type);
         myRoomBuilder.setRoomType();
-
     }
 
-    public static void main(String[] args){
-
-        RoomDirectorBuilder rdb = new RoomDirectorBuilder();
-
-        rdb.buildRoom("a1", "F", "ClassRoom");
-
-        Room room = rdb.getRoom();
-
-        room.setSeats(12);
-        room.setBoard("lavagna nera");
-
-        System.out.println(
-                " Id: "  + room.getId() +
-                " Name: " + room.getName() +
-                " Building: " + room.getBuilding() +
-                " Seats: " + room.getSeats() +
-                " Board: " + room.getBoard() +
-                " Type: " + room.getType());
-
+    /**
+     * This method get the current room instance
+     *
+     * @return Room
+     */
+    public Room getRoom() {
+        return myRoomBuilder.getRoom();
     }
-
 }
