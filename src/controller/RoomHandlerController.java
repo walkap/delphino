a42138566b1c7653c9b4dac04f807845629a4910
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-14 23:24:35 +0100

    created some basic methods in RoomHandlerController

--- src/controller/RoomHandlerController.java
+++ src/controller/RoomHandlerController.java
@@ -1,7 +1,44 @@
 package controller;
 
+import dao.RoomDao;
+import entity.room.Room;
+
+import java.util.Vector;
+
 public class RoomHandlerController {
 
+    private RoomDao rd = RoomDao.getInstance();
 
+    public void addNewRoom(Room room){
+        if(room.hasTeacherDesk() == null){
+            room.setTeacherDesk(false);
+        }
+        rd.addRoom(room);
+    }
+
+    public void deleteRoom(Room room){
+        rd.deleteRoom(room);
+    }
+
+    public void updateRoom(Room room){
+        rd.updateRoom(room);
+    }
+
+    public Room getRoom(Room room){
+        return rd.getRoom(room);
+    }
+
+    public Vector<Room> getAllRooms(){
+        return rd.getAllRooms();
+    }
+
+    /*public static void main(String[] args) {
+        RoomHandlerController rhc = new RoomHandlerController();
+
+        Room room = new Room("D15", "5", "Laboratory");
+
+        rhc.addNewRoom(room);
+
+    }*/
 
 }
