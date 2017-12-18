b32a76a8bcd471f8a02cc4f113a3e1e76d165cc9
riccardo <riccardogamucci@alice.it>
2017-12-18 18:32:47 +0100

    Macroareas

---
+++ src/util/Area.java
@@ -0,0 +1,11 @@
+package util;
+
+public class Area {
+
+    private static final String [] AREAS = {"Lettere e Filosofia", "Ingegneria",
+            "Scienze Matematiche", "Medicina", "Economia", "Giurisprudenza"};
+
+    public  static String[] getAreas() {
+        return AREAS;
+    }
+}
