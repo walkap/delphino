75e6911b32fef43d21417eea9c4ec34ddeb45cc2
riccardo <riccardogamucci@alice.it>
2017-12-18 18:37:28 +0100

    Updating Building Controller

--- src/controller/BuildingController.java
+++ src/controller/BuildingController.java
@@ -1,24 +1,35 @@
 package controller;
 
-import entity.Building;
 import dao.BuildingDao;
-import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import entity.Building;
 
 public class BuildingController {
-    @FXML
-    private TextField name;
-    @FXML
-    private TextField area;
 
-    public void createBuilding() {
+    public void createNewBuilding(String name, String area) {
 
-        String nameBuilding = name.getText();
-        String nameArea = area.getText();
-
-        Building building = new Building(nameBuilding, nameArea);
         BuildingDao buildingDao = new BuildingDao();
-        buildingDao.addBuilding(building);
+        buildingDao.addBuilding(new Building(name, area));
+
+
+    }
+
+    public void modifyBuilding(String oldName, String oldArea, String newName, String newArea) {
+
+        Building oldBuilding = new Building(oldName, oldArea);
+        Building newBuilding = new Building(newName, newArea);
+
+        BuildingDao buildingDao = new BuildingDao();
+        buildingDao.updateBuilding(oldBuilding, newBuilding);
+    }
+
+    public void deleteBuilding(String name, String area) {
+
+        Building building = new Building(name, area);
+
+        BuildingDao buildingDao = new BuildingDao();
+        buildingDao.deleteBuilding(building);
+
+
     }
 
 }
