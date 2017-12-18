5cbbba285e3dbc9724a55e3f1d7573c3acd18616
Luca <l.atturo@hotmail.it>
2017-12-18 23:01:23 +0100

    create main package with main class. now it is possible go in other activities

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,22 +1,25 @@
-package boundary.templateRoom.modifyDeleteTemplateRoom;
+package main;
 
 import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainModifyDeleteTemplateRoom extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("ModifyDeleteTemplateRoom.fxml"));
-        primaryStage.setTitle("Modifica o Elimina Modello Stanza");
+        Parent root = FXMLLoader.load(getClass().getResource("MainUI.fxml"));
+        primaryStage.setTitle("Main");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
 
     }
+
+
     public static void main(String[] args) {
         launch(args);
     }
