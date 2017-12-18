2eba9b7f08ada0445a0b4ba763e5f4acd7c4a5e7
riccardo <riccardogamucci@alice.it>
2017-12-18 18:38:13 +0100

    Building Main

---
+++ src/boundary/buildingBoundary/MainBuilding.java
@@ -0,0 +1,26 @@
+package buildingBoundary;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainBuilding extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("buildingUI.fxml"));
+
+        primaryStage.setTitle("Crea e Modifica Edificio");
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
