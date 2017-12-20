ac19e8edd3d66ca1be53dcf0e4f001e20fa863f2
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-20 10:37:23 +0100

    new main for addnewRoomboundary

---
+++ src/boundary/addNewRoomBoundary/Main.java
@@ -0,0 +1,25 @@
+package boundary.addNewRoomBoundary;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("addNewRoomView.fxml"));
+        primaryStage.setTitle("Add new room");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
