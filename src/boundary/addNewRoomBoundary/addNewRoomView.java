3fd51fa22e950564f01cdb9d03ac3938fba8b988
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-20 11:30:06 +0100

    add new room from template

--- src/boundary/addNewRoomBoundary/addNewRoomView.java
+++ src/boundary/addNewRoomBoundary/addNewRoomView.java
@@ -1,10 +1,12 @@
 package boundary.addNewRoomBoundary;
 
 import controller.RoomHandlerController;
+import entity.TemplateRoom;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.util.StringConverter;
 import util.Types;
 
 public class addNewRoomView {
@@ -17,6 +19,11 @@ public class addNewRoomView {
     @FXML
     private ComboBox<Integer> roomBuilding;
     private ObservableList<Integer> buildingsList = FXCollections.observableArrayList();
+
+    @FXML
+    private ComboBox<TemplateRoom> roomTemplate;
+    private ObservableList<TemplateRoom> roomTemplateList = FXCollections.observableArrayList();
+
     @FXML
     private TextField roomSeats;
     @FXML
@@ -33,6 +40,26 @@ public class addNewRoomView {
         String[] types = Types.getTypes();
         Integer[] buildings = {1,2,3};
 
+        //Initialize template combo
+        roomTemplateList.add(new TemplateRoom("ciccio", 200, "nera", 2, 20, true));
+        roomTemplate.setItems(roomTemplateList);
+
+        //Convert type from template to string, where string is the template name
+        roomTemplate.setConverter(new StringConverter<TemplateRoom>() {
+            @Override
+            public String toString(TemplateRoom object) {
+                if(object == null){
+                    return null;
+                }else{
+                    return object.getNameTemplate();
+                }
+            }
+            @Override
+            public TemplateRoom fromString(String string) {
+                return null;
+            }
+        });
+
         //Initialize type combo
         typesList.addAll(types);
         roomType.setItems(typesList);
@@ -40,6 +67,19 @@ public class addNewRoomView {
         //Initialize building combo
         buildingsList.addAll(buildings);
         roomBuilding.setItems(buildingsList);
+    }
+
+    /**
+     * This template fill most fields
+     * to simplify the insert
+     */
+    public void fillFromTemplate(){
+        TemplateRoom template = roomTemplate.getValue();
+        roomSeats.setText(Integer.toString(template.getSeats()));
+        roomBoard.setText(template.getBoard());
+        roomProjectors.setText(Integer.toString(template.getProjectors()));
+        roomComputers.setText(Integer.toString(template.getComputers()));
+        roomTeacherDesk.setSelected(template.getDesk());
     }
 
     /**
