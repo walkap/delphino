ee43261f36655b63a985b1a88fe8a52c8a51e51c
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-20 10:23:31 +0100

    created util to have the room's types list

---
+++ src/util/Types.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Types {
+
+    private static final String[] TYPES = { "ClassRoom", "Laboratory", "CongressHall" };
+
+    public static String[] getTypes(){
+        return TYPES;
+    }
+}
