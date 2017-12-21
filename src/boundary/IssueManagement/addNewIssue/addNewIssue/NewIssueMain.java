70e3950fbade11e5fddd0bf426e4d4cb85f1efb7
riccardo <riccardogamucci@alice.it>
2017-12-21 20:44:27 +0100

    Issue controller, UI and NewIssueMain updated

---
+++ src/boundary/IssueManagement/addNewIssue/addNewIssue/NewIssueMain.java
@@ -0,0 +1,26 @@
+package boundary.IssueManagement.addNewIssue.addNewIssue;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class NewIssueMain extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("IssueUI.fxml"));
+
+        primaryStage.setTitle("Add new Issue");
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
