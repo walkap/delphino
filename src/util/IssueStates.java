e62385991c460ff02e7950dd237d4af539b1b795
riccardo <riccardogamucci@alice.it>
2017-12-21 20:42:03 +0100

    Issue states added to util

---
+++ src/util/IssueStates.java
@@ -0,0 +1,12 @@
+package util;
+
+public class IssueStates {
+
+
+    private static final String [] STATES = {"New", "Not Resolvable", "Assigned",
+                            "In Testing", "Resolved"};
+
+    public static String[] getSTATES() {
+        return STATES;
+    }
+}
