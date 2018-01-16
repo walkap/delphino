eace81f1bf8dd5b6b94bfff2c1eb7aff6b2c4983
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-01-16 23:21:30 +0100

    added comments to the roomdao interface

--- src/dao/room/RoomDao.java
+++ src/dao/room/RoomDao.java
@@ -7,16 +7,56 @@ import java.util.Vector;
 
 public interface RoomDao {
 
+    /**
+     * This abstract method insert a new room in the persistence
+     *
+     * @param room - Room
+     * @throws SQLException
+     */
     void insertRoom(Room room) throws SQLException;
 
+    /**
+     * This abstract method delete a room from the persistence
+     *
+     * @param room - Room
+     */
     void deleteRoom(Room room);
 
+    /**
+     * This abstract method update a room from the persistence if present
+     *
+     * @param room - Room
+     */
     void updateRoom(Room room);
 
+    /**
+     * This abstract method returns a room from the persistence if present
+     *
+     * @param name - String
+     * @return Room
+     */
     Room getRoom(String name);
 
+    /**
+     * This abstract method returns a vector of rooms from the persistence
+     *
+     * @return Vector
+     */
     Vector<Room> getAllRooms();
 
+    /**
+     * This abstract methof returns a vector of rooms from the persistence
+     * where some conditions are satisfied
+     *
+     * @param type        - String
+     * @param building    - int
+     * @param board       - String
+     * @param teacherDesk - boolean
+     * @param seats       - int
+     * @param projectors  - int
+     * @param computers   - int
+     * @return Vector
+     */
     Vector<Room> getRooms(String type, int building, String board, boolean teacherDesk, int seats, int projectors, int computers);
 
 }
