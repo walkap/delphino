f4c83d1663d4f1065d04f769fab257230f7970f3
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-01-24 22:34:38 +0100

    new user entity

---
+++ src/entity/user/User.java
@@ -0,0 +1,56 @@
+package entity.user;
+
+public class User {
+
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String type;
+
+    public User(String name, String surname, String email){
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
