02bf40cf686aacfb8fe86bc6b5ee31b14bb940ff
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-01-28 22:01:32 +0100

    added first unit test

---
+++ test/control/RoomManagementControllerTest.java
@@ -0,0 +1,32 @@
+package control;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomManagementControllerTest {
+
+    @Test
+    void insertRoom() {
+    }
+
+    @Test
+    void deleteRoom() {
+    }
+
+    @Test
+    void updateRoom() {
+    }
+
+    @Test
+    void getRoomByName() {
+    }
+
+    @Test
+    void getAllRooms() {
+    }
+
+    @Test
+    void getRooms() {
+    }
+}
