f3f7d7de4c49cc7af2fbd07747f52be672c054f5
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-02-01 23:43:22 +0100

    used the factory to call the dao

--- src/control/LoginController.java
+++ src/control/LoginController.java
@@ -1,16 +1,14 @@
 package control;
 
+import dao.factory.DaoFactory;
 import dao.user.UserDao;
-import dao.user.UserDaoDb;
 import entity.user.User;
 import exception.user.LoginException;
 import javafx.fxml.LoadException;
 import util.EmailValidator;
 
 public class LoginController {
-
-    private UserDao userDao = new UserDaoDb();
-
+    private DaoFactory dbFactory = DaoFactory.getDaoFactory(DaoFactory.DATABASE);
     /**
      * This method is used to return a user if present in the database
      *
@@ -23,12 +21,11 @@ public class LoginController {
         if (email == null || password == null) {
             throw new LoginException("Email and password are mandatory!");
         }
-
         EmailValidator emailValidator = new EmailValidator();
         if (!emailValidator.isValidEmail(email)) {
             throw new LoginException("Invalid email address!");
         }
-
+        UserDao userDao = dbFactory.getUserDao();
         User user = null;
         try {
             user = userDao.getUser(email, password);
