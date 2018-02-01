86a9e8090fb766926a726bc4b5eba0d6c84b95aa
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-02-01 23:24:43 +0100

    added override annotation

--- src/dao/factory/DbDaoFactory.java
+++ src/dao/factory/DbDaoFactory.java
@@ -6,11 +6,11 @@ import dao.room.RoomDao;
 import dao.room.RoomDaoDb;
 
 public class DbDaoFactory extends DaoFactory{
-
-
+    @Override
     public RoomDao getRoomDao() {
         return new RoomDaoDb();
     }
+    @Override
     public FeatureDao getFeatureDao() {
         return new FeatureDaoDb();
     }
