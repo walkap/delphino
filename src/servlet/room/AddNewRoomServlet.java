976b0b01fc39fdb40da80f8222a15033775c5548
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-02-01 23:31:52 +0100

    some improvements

--- src/servlet/room/AddNewRoomServlet.java
+++ src/servlet/room/AddNewRoomServlet.java
@@ -13,22 +13,20 @@ import java.io.IOException;
 @WebServlet(name = "AddNewRoomServlet")
 public class AddNewRoomServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
         int computers = 0;
         int seats = 0;
         int projectors = 0;
         boolean hasDesk;
-
-        response.setContentType("text/html");
+        //Get data from the form
         String name = request.getParameter("name");
         String building = request.getParameter("building");
         String area = request.getParameter("area");
         String type = request.getParameter("type");
         String board = request.getParameter("board");
-
         if (!request.getParameter("computers").isEmpty()) {
             computers = Integer.parseInt(request.getParameter("computers"));
         }
-
         if (!request.getParameter("seats").isEmpty()) {
             seats = Integer.parseInt(request.getParameter("seats"));
         }
@@ -36,16 +34,17 @@ public class AddNewRoomServlet extends HttpServlet {
             projectors = Integer.parseInt(request.getParameter("projectors"));
         }
         hasDesk = request.getParameter("hasDesk") != null;
+        //Instantiate the controller
         HandleRoom handleRoom = new HandleRoom();
         try {
+            //Insert the room
             handleRoom.insertRoom(name, type, building, area, board, hasDesk, seats, projectors, computers);
+            //Redirect the user to the same page refreshed
             response.sendRedirect("/pages/add-new-room.jsp");
-        }catch (InsertRoomException e){
+        } catch (InsertRoomException e) {
             e.printStackTrace();
         }
-
     }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
